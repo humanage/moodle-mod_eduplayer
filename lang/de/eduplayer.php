ff0853a4de3b21e5427c6bc2f26e59dc37e85fc7
Guido Roessling <guido@informatik.tu-darmstadt.de>
2014-02-19 08:28:08 +0100

    German "formal" language file for EduPlayer
    Note: the precious string key "url", which caused a type clash in some databases with key "URL", was renamed to "fullurl".

---
+++ lang/de/eduplayer.php
@@ -0,0 +1,117 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://moodle.informatik.tu-darmstadt.de
+ *
+ * @package    mod
+ * @subpackage eduplayer
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['appearance'] = 'Aussehen';
+$string['audio'] = 'Audio';
+$string['autostart'] = 'Automatischer Start';
+$string['behaviour'] = 'Verhalten';
+$string['cancel'] = 'Abbrechen';
+$string['captions'] = 'Untertitel';
+$string['captionsback'] = 'Transparenter Hintergrund';
+$string['captionsfile'] = 'Datei für Untertitel';
+$string['captionsfile_help'] = 'Datei für Untertitel: nur .vtt, .srt oder .xml';
+$string['captionsfontsize'] = 'Schriftgröße';
+$string['captionsstate'] = 'Untertitel anzeigen';
+$string['controls'] = 'Kontrollen';
+$string['disclaimer'] = 'Anzuzeigende Warnung, wenn das Medium Copyrightschutz unterliegt';
+$string['disclaimerlabel'] = 'Anzuzeigende Warnung, wenn das Medium Copyrightschutz unterliegt';
+$string['disclaimer_help'] = 'Achtung: diese Warnung wird nur angezeigt, wenn das Feld nicht leer ist';
+$string['download'] = 'Herunterladen';
+$string['downloadenabled'] = 'Herunterladen von Medien aktivieren';
+$string['downloadfile'] = 'Medien herunterladen';
+$string['eduplayer'] = 'EduPlayer';
+$string['eduplayer:addinstance'] = 'Instanz hinzufügen';
+$string['eduplayerappearance'] = 'Aussehen';
+$string['eduplayerappearance_help'] = 'Definieren der Größe und des Aussehens des Players, z.B. Breite, Höhe, Skin und Bild';
+$string['eduplayerbehaviour'] = 'Verhalten';
+$string['eduplayerbehaviour_help'] = 'Auswahl wie das Video gerändert und wieder gegeben werden soll (ohne Ton, Autostart, ...)';
+$string['eduplayercaptions'] = 'Untertitel';
+$string['eduplayercaptions_help'] = 'Unterstützung für Untertitel aktivieren';
+$string['eduplayerfieldset'] = 'Beispieleinstellungen anpassen';
+$string['eduplayerfile'] = 'Datei';
+$string['eduplayerfile_help'] = 'Es können Videodateien im Format .mp4, .flv und .webm sowie Audiodateien im Format .mp3 und .m4a hochgeladen werden.';
+$string['eduplayername'] = 'Name des EduPlayers';
+$string['eduplayername_help'] = 'Geben Sie hier den Namen der Aktivität ein';
+$string['eduplayerplaylist'] = 'Wiedergabeliste';
+$string['eduplayerplaylist_help'] = 'Wählen Sie die Position und Größe (in Pixeln) der Wiedergabeliste';
+$string['eduplayerrepeat'] = 'Wiederholen';
+$string['eduplayerskin'] = 'Skin des Players';
+$string['eduplayersource'] = 'Quelle';
+$string['eduplayersource_help'] = 'Es können Videodateien im Format .mp4, .flv und .webm sowie Audiodateien im Format .mp3 und .m4a hochgeladen werden oder eine URL zu einer Mediendatei mit den gleichen Endungen angegeben werde\
+n.';
+$string['emailnotcorrect'] = 'Ungültige Email-Adresse';
+$string['emailnotsent'] = 'Fehler beim Versenden der Email';
+$string['emailsent'] = 'Email gesendet an {$a}!';
+$string['false'] = 'Nein';
+$string['FILE'] = 'DATEI';
+$string['height'] = 'Höhe';
+$string['image'] = 'Bild des Veröffentlichers';
+$string['image_help'] = 'Bild des Veröffentlichers: nur .jpg, .jpeg oder .png';
+$string['linkurl'] = 'Link';
+$string['modulename'] = 'EduPlayer';
+$string['modulenameplural'] = 'EduPlayer';
+$string['modulename_help'] = 'Das EduPlayer-Module erlaubt die Wiedergabe von Videos im Format .mp4 und .flv sowie Audio im Format .mp3 oder .m4a';
+$string['mute'] = 'Lautlos';
+$string['notes'] = 'Notizen';
+$string['playlist'] = 'Position';
+$string['playlistpositionbottom'] = 'Unten';
+$string['playlistpositionnull'] = 'Keine';
+$string['playlistpositionright'] = 'Rechts';
+$string['playlists'] = 'Wiedergabeliste';
+$string['playlistsize'] = 'Größe (in Pixeln)';
+$string['pluginadministration'] = 'Administration des EduPlayers';
+$string['pluginname'] = 'EduPlayer';
+$string['rtmp'] = 'RTMP-Streaming';
+$string['share'] = 'Teilen';
+$string['shareemaillabel'] = 'Email-Adresse';
+$string['sharelink'] = 'Zu teilender Link';
+$string['sharelink_help'] = 'Achtung: die Weitergabe der Email wird durch Ausfüllen dieses Feldes aktiviert. Wenn das Feld leer ist, wird der Link nicht per Email versendet.';
+$string['sharemailmessage_editor'] = 'Nachricht zum Teilen per Email';
+$string['sharemailmessage_editor_help'] = 'Dieser Text wird als Nachrichtenkörper der Email zusammen mit dem Link im "Teilen"-Feld versendet. Bitte ändern Sie den Inhalt nach Belieben.';
+$string['sharemessage'] = '<p>Sehen Sie sich diesen interessanten Kurs an:<br /></p>';
+$string['sharemessagelabel'] = 'Nachricht zum Teilen per Email';
+$string['sharesubbject'] = '{$a->firstname} {$a->lastname} teilt das folgende Multimedia-Material mit Ihnen';
+$string['sharetext'] = 'Senden Sie einen Link per Email zu einer Online-Kopie dieser Ressource';
+$string['sharing'] = 'Teilen';
+$string['stretching'] = 'Inhalt auf die Breite des Player strecken';
+$string['stretchingexactfit'] = 'Inhalt so anpassen, dass er in den Player passt';
+$string['stretchingfill'] = 'Player ausfüllen, ohne den Inhalt zu strecken';
+$string['stretchingnone'] = 'Keine Anpassung';
+$string['stretchinguniform'] = 'Player an Inhalt anpassen';
+$string['title'] = 'Titel';
+$string['true'] = 'Ja';
+$string['type'] = 'Wiedergabetyp';
+$string['URL'] = 'URL';
+$string['fullurl'] = 'Vollständige URL';
+$string['urltype'] = 'Typ';
+$string['validemail'] = 'Bitte geben Sie eine gültige Email-Adresse an';
+$string['video'] = 'Video';
+$string['vimeo'] = 'Vimeo';
+$string['vplaylist'] = 'Video-Wiedergabeliste';
+$string['width'] = 'Breite';
+$string['youtube'] = 'YouTube';
+$string['ytplaylist'] = 'YouTube-Wiedergabeliste';
